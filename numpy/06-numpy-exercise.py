2301f2e34ac18ffdc96c972575636b05ff4c0374
Waihon Yew <yewwaihon@gmail.com>
2024-10-20 23:47:59 +0800

    Create 06-numpy-exercise.py

---
+++ numpy/06-numpy-exercise.py
@@ -0,0 +1,18 @@
+import numpy
+
+arrayOne = numpy.array([
+    [5, 6, 9],
+    [21, 18, 27]
+])
+
+arrayTwo = numpy.array([
+    [15, 33, 24],
+    [4, 7, 1]
+])
+
+resultArray = arrayOne + arrayTwo
+print(resultArray)
+
+for num in numpy.nditer(resultArray, op_flags = ["readwrite"]):
+    num[...] = num * num
+print(resultArray)
