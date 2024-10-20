2bf49d87355ef0f5e9c84d2b98e749e5510b8e91
Waihon Yew <yewwaihon@gmail.com>
2024-10-20 21:18:11 +0800

    Fix income tax calculation

--- basic-exercises/12-calculate-income-tax.py
+++ basic-exercises/12-calculate-income-tax.py
@@ -2,17 +2,19 @@ def income_tax(income):
     income_tax = 0
     remaining = income
     
-    if remaining >= 10_000:
-        income_tax += 10_000 * 0.0
-        remaining -= 10_000
-
-    if remaining >= 10_000:
-        income_tax += 10_000 * 0.10
-        remaining -= 10_000
-
-    income_tax += remaining * 0.20
+    limit = [ 10_000, 10_000, 9_999_999 ]
+    rates = [ 0.0, 0.10, 0.20 ]
+    size = len(limit)
+    
+    for i in range(size):
+        taxable_income = min(remaining, limit[i])
+        income_tax += taxable_income * rates[i]
+        remaining -= taxable_income
+        if remaining <= 0:
+            return income_tax
     
     return income_tax
 
-income = 45_000
-print("Income tax for ${:.0f}: ${:.0f}".format(income, income_tax(income)))
+incomes = [9_999, 10_000, 19_999, 20_000, 45_000]
+for income in incomes:
+    print("Income tax for ${:.0f}: ${:.2f}".format(income, income_tax(income)))
