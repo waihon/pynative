62e05f4305c4a34a90053abed0532c96a10c22ef
Waihon Yew <yewwaihon@gmail.com>
2024-10-21 00:22:23 +0800

    Update 07-numpy-exercise.py

--- numpy/07-numpy-exercise.py
+++ numpy/07-numpy-exercise.py
@@ -4,9 +4,11 @@ sampleArray = numpy.array([[34,43,73],[82,22,12],[53,94,66]])
 print(sampleArray)
 
 # Sorting original array by 2nd row
+print(sampleArray[1,:].argsort())
 sortedArrayByRow = sampleArray[:,sampleArray[1,:].argsort()]
 print(sortedArrayByRow)
 
 # Sorting original array by 2nd column
+print(sampleArray[:,1].argsort())
 sortedArrayByColumn = sampleArray[sampleArray[:,1].argsort()]
 print(sortedArrayByColumn)
